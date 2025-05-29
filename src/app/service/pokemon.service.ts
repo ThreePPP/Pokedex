2f454488cc7b0b24fc5ec667a9002b917e4a3d21
ThreePPP <131146098+ThreePPP@users.noreply.github.com>
2025-05-29 14:23:03 +0700

    edit 8
    3030 to 4444

--- src/app/service/pokemon.service.ts
+++ src/app/service/pokemon.service.ts
@@ -11,7 +11,7 @@ export class PokemonService {
   constructor(private http: HttpClient) {}
 
   getPokemonList(limit = 20, name = ''): Observable<any[]> {
-    const url = `http://localhost:3030/api/cards?name=${name}&limit=${limit}`;
+    const url = `http://localhost:4444/api/cards?name=${name}&limit=${limit}`;
     return this.http.get<{ cards: any[] }>(url).pipe(map(res => res.cards));
   }
 }
